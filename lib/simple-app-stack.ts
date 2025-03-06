6f2896866bf6bc118dcdac083a8c29f97051e2be
JIAYI SHAO <sjy08015342@163.com>
2025-03-06 15:27:29 +0000

    Lambda function URLs.

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,12 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 import { Construct } from 'constructs';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // 创建 Lambda
     const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.NODEJS_22_X,
@@ -14,5 +16,16 @@ export class SimpleAppStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(10),
       memorySize: 128,
     });
+
+    // 添加 Function URL（公开访问）
+    const simpleFnURL = simpleFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE, // 公开访问
+      cors: {
+        allowedOrigins: ["*"], // 允许所有来源
+      },
+    });
+
+    // 输出 URL
+    new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
   }
 }
